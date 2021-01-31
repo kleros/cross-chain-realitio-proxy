de34fdbf70a2b1ed08d87bf9b2a0270c4d6be339
unknownunknown1 <time_arrow@bk.ru>
2021-01-31 19:52:42 +1000

    docs: added myself to reviewers

--- contracts/src/RealitioForeignArbitrationProxy.sol
+++ contracts/src/RealitioForeignArbitrationProxy.sol
@@ -2,7 +2,7 @@
 
 /**
  *  @authors: [@hbarcelos]
- *  @reviewers: [@ferittuncer*, @fnanni-0, @nix1g, @epiqueras*, @clesaege*]
+ *  @reviewers: [@ferittuncer*, @fnanni-0, @nix1g, @epiqueras*, @clesaege*, @unknownunknown1]
  *  @auditors: []
  *  @bounties: []
  *  @deployments: []

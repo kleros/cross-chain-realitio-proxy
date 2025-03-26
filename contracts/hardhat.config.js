6dbbe2dc003bc304742196cf9d733fd0a4b97882
jaybuidl <jb@kleros.io>
2025-03-26 14:57:06 +0000

    chore: removed the 0.7 solc config

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -11,15 +11,6 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.7.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
         version: "0.8.25",
         settings: {
           optimizer: {

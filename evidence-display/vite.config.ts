3334ac41df50f0b98dfcc37ed0f8d6fb2b5eecbf
jaybuidl <jb@kleros.io>
2025-04-01 12:03:10 +0100

    fix: use relative paths in evidence display build output

--- evidence-display/vite.config.ts
+++ evidence-display/vite.config.ts
@@ -4,6 +4,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react(), nodePolyfills()],
   build: {
     outDir: "dist",

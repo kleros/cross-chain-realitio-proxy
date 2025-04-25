24a596b985f40666c7831574e092199b0cadf7d0
alcercu <333aleix333@gmail.com>
2025-04-25 12:45:54 +0200

    chore: use ascii charset

--- dynamic-script/vite.config.ts
+++ dynamic-script/vite.config.ts
@@ -4,6 +4,9 @@ import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    charset: "ascii",
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
